e68f127f4a240737d70b53cd21b5564753cef37a
Gabriel Wewerka <111596628+gabewerk@users.noreply.github.com>
2024-12-13 13:21:36 -0500

    Add files via upload
    new optical.py

--- optical.py
+++ optical.py
@@ -3,8 +3,10 @@ import os
 import cv2
 from ultralytics import YOLO
 
-raw_optical_path = "/Users/werk/Documents/Code/aspire_explosive_detection/optical_sensor/optical_data" # folder path
+raw_optical_path = "/Users/gabe/Documents/Code/optical_data" # folder path
 os.chdir(raw_optical_path) # change the directory
+MODEL_NAME = "yolo11n.pt" 
+model = YOLO(MODEL_NAME) # load YOLO
 
 def read_image(input):
     image = cv2.imread(input)
@@ -17,22 +19,16 @@ def read_image(input):
 
 # reads the images from the directory
 for file in os.listdir():
-    if file.endswith(".jpeg"): # check formatting
+    if file.endswith(".png"): # check formatting
         file_path = f"{raw_optical_path}/{file}"
+        results = model(source=file_path, show=False, save=True, show_labels=False, show_conf=False)
         image = read_image(file_path)
+        # display the image using OpenCV's built-in viewer
+        #cv2.imshow('Loaded Image', image)
+        #cv2.waitKey(0)  # wait until any key is pressed
+        #cv2.destroyAllWindows()  # close the window
 
-# object detection
-MODEL_NAME = "yolo11n.pt"
-# load YOLO
-model = YOLO(MODEL_NAME)
-# train the model
-#results = model.train(dat="coco8.yaml", epochs=100, imgsz=640)
-results = model(source=raw_optical_path + "/image.jpeg")
+#trained_model = model.train(dat="coco8.yaml", epochs=100, imgsz=640) # train the model
+# for individual images
+#results = model(source=raw_optical_path + "/image_2931075238.png", show=True, save=False, conf=0.001, show_labels=False)
 
-print(f"Shape: {image.shape}")  # print the shape of image. (height, width, color channels)
-#print(f"Pixel [0,0]: {image[0,0]}") # print the contents of pixel at [0,0]
-
-# display the image using OpenCV's built-in viewer
-cv2.imshow('Loaded Image', image)
-cv2.waitKey(0)  # wait until any key is pressed
-cv2.destroyAllWindows()  # close the window
